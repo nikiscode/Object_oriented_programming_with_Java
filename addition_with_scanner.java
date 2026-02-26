52df1fe8e8c3ea9551fb2d5bde4f71f14a5a7155
Nikitha N <nikitha.nrb@gmail.com>
2026-02-26 22:13:30 +0530

    Create addition_with_scanner.java

---
+++ addition_with_scanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class add {
+    public static void main (String[] add){
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the First Number: ");
+        int num1=scanner.nextInt();
+
+        System.out.println("Enter the Second Number: ");
+        int num2=scanner.nextInt();
+
+        int sum=num1 + num2;
+
+        System.out.println("Sum is: " + sum);
+
+        scanner.close();
+    }
+
+}
