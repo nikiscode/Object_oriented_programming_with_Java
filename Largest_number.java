e11d4af42994e63c4fe104ff975eb64c9008ed98
Nikitha N <nikitha.nrb@gmail.com>
2026-02-28 23:35:34 +0530

    Create Largest_number.java

---
+++ Largest_number.java
@@ -0,0 +1,13 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println("A is largest");
+        } else {
+            System.out.println("B is largest");
+        }
+    }
+}
