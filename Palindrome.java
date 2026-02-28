f0e6dd1a8bec4f0eb8667b45c767eef83113021e
Nikitha N <nikitha.nrb@gmail.com>
2026-02-28 23:31:34 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,20 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
 
+        int num = 121;   // You can change the number here
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println(original + " is a Palindrome number.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome number.");
+        }
+    }
+}
