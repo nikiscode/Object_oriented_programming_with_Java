0200ffc1340f81605a0a7d1a380cc32443259714
Nikitha N <nikitha.nrb@gmail.com>
2026-02-28 23:34:08 +0530

    Update Substraction.java

--- Substraction.java
+++ Substraction.java
@@ -1,1 +1,11 @@
+public class Subtraction {
+    public static void main(String[] args) {
 
+        int a = 10;
+        int b = 5;
+
+        int result = a - b;
+
+        System.out.println("Subtraction = " + result);
+    }
+}
