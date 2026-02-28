5c66b0348395f8a222803b22c5016de23dcdd570
Nikitha N <nikitha.nrb@gmail.com>
2026-02-28 23:33:06 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,1 +1,13 @@
+public class Division {
+    public static void main(String[] args) {
 
+        int a = 20;
+        int b = 5;
+
+        int result = a / b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Division Result: " + result);
+    }
+}
