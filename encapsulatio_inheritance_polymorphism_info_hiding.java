c0ca67bba44c3ed4303aea3761b9215218993cc7
Nikitha N <nikitha.nrb@gmail.com>
2026-03-12 12:23:39 +0530

    Create encapsulatio_inheritance_polymorphism_info_hiding.java

---
+++ encapsulatio_inheritance_polymorphism_info_hiding.java
@@ -0,0 +1,4 @@
+Encapsulation:Wrapping of code and data together into single unit {private,protected,public}
+Inhertance:Mechanism in which one object acquires all the properties and behaviours of a parent object .It is an import part of OOP'S (object oriented programming).
+Information hidding
+Polymorphism:Language ability to process objects depending on their class 
