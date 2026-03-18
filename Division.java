e2c6d0ee9604d26b3d48a8a3c9b84989da1fad2d
Nikitha N <nikitha.nrb@gmail.com>
2026-03-18 21:52:10 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,12 +1,10 @@
 //Division without scanner statement 
+ 
 public class Division {
-    public static void main(String[] args) {
-
+public static void main(String[] args) {
         int a = 20;
         int b = 5;
-
         int result = a / b;
-
         System.out.println("First Number: " + a);
         System.out.println("Second Number: " + b);
         System.out.println("Division Result: " + result);
