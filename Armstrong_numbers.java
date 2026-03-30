ab2dbf69b759067abfff9239e9c466ec93b1092f
Nikitha N <nikitha.nrb@gmail.com>
2026-03-30 22:41:42 +0530

    Create Armstrong_numbers.java

---
+++ Armstrong_numbers.java
@@ -0,0 +1,17 @@
+\\Armstrong Number:
+public class Armstrong {
+public static void main(String[] args) {
+int num = 153, sum = 0, temp = num;
+while (temp != 0) {
+int digit = temp % 10;
+sum += digit * digit * digit;
+temp /= 10;
+}
+if (sum == num)
+System.out.println("Armstrong Number");
+else
+System.out.println("Not Armstrong Number");
+}
+}
+Output:
+Armstrong Number
