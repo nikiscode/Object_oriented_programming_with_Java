f1a421bb730ae84f688c81b597faae5cc52b6656
Nikitha N <nikitha.nrb@gmail.com>
2026-03-30 22:44:28 +0530

    Create Binary_search.java

---
+++ Binary_search.java
@@ -0,0 +1,12 @@
+\\Binary Search
+public class BinarySearch {
+public static void main(String[] args) {
+int arr[] = {1, 2, 3, 5, 9};
+int key = 5;
+int low = 0, high = arr.length - 1;
+6. Binary Search
+public class BinarySearch {
+public static void main(String[] args) {
+int arr[] = {1, 2, 3, 5, 9};
+int key = 5;
+int low = 0, high = arr.length - 1;
