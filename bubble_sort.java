dbeaed29777ecd467f7ec614e282a3c8189d1cf3
Nikitha N <nikitha.nrb@gmail.com>
2026-03-30 22:43:40 +0530

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -20,9 +20,3 @@ System.out.print(num + " ");
 Output:
 Sorted Array:
 1 2 3 5 9
-6. Binary Search
-public class BinarySearch {
-public static void main(String[] args) {
-int arr[] = {1, 2, 3, 5, 9};
-int key = 5;
-int low = 0, high = arr.length - 1;
