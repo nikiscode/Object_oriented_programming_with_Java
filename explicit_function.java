48604757fbbf4ff2e76124d269a00c8d8be6a1bf
Nikitha N <nikitha.nrb@gmail.com>
2026-04-01 19:19:22 +0530

    Create explicit_function.java

---
+++ explicit_function.java
@@ -0,0 +1,9 @@
+Explicit function
+package com.dataflair.typeconversion;
+public class ExplicitProblem {
+public static void main(String[] args) {
+int a=57;
+byte b =(byte)a;
+System.out.print(b);
+}
+}
